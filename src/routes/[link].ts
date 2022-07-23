2fba4ee3ad0a72cc5926a3e30851923f8e9263b6
Keerthi Vasan S A <sakeerthi23@gmail.com>
2022-07-23 20:53:30 +0530

    added cache headers, removed supabase function invoke

--- src/routes/[link].ts
+++ src/routes/[link].ts
@@ -6,12 +6,10 @@ export const GET: RequestHandler = async ({ params }) => {
 	let data = await supabase.from('links').select('url').eq('link', link);
 	if (data.error || !data.body || data.body.length < 1)
 		return { headers: { location: '/' }, status: 302 };
-
-	supabase.rpc('increment', { id: data.body[0].id });
-
 	return {
 		headers: {
-			location: data.body[0].url
+			location: data.body[0].url, 
+			'Cache-control':'public, max-age=5184000, immutable, stale-while-revalidate=8640000',
 		},
 		status: 302,
 		body: 'Redirecting...'
