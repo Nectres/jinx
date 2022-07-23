d5f1ef95fadb714f2c53bcb8ecfcc793aa1733ae
Keerthi Vasan S A <sakeerthi23@gmail.com>
2022-07-23 19:21:54 +0530

    process env

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -2,6 +2,6 @@
 import { createClient } from "@supabase/supabase-js"
 
 const supabaseUrl = 'https://nfhskpsofmdccutmxogf.supabase.co'
-const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY ?? process.env.VITE_SUPABASE_ANON_KEY
+const supabaseKey = process.env.VITE_SUPABASE_ANON_KEY ?? import.meta.env.VITE_SUPABASE_ANON_KEY 
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
